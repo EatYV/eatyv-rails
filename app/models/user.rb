383e7a9f409b2b800fdb2d395ec902b66271e6f5
Salanoid <salanoid@gmail.com>
2017-08-02 21:38:50 +0000

    add recipes that belongs to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
          
   validates :city, :address, presence: true
+  
+  has_many :recipes
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170802203637) do
+ActiveRecord::Schema.define(version: 20170802213255) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "title"
+    t.text "recipe"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.string "city"
+    t.string "address"
+    t.string "zipcode"
+    t.float "latitude"
+    t.float "longitude"
+    t.index ["user_id"], name: "index_recipes_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -35,4 +49,5 @@ ActiveRecord::Schema.define(version: 20170802203637) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "recipes", "users"
 end
